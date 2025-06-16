bdd0d7e5a3787eb64320bd415745e216c54d61e2
DaniExp <dani00exp@gmail.com>
2025-06-16 22:53:54 +0200

    Add test for ft_strcpy + free

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 
 int main() {
 	helloWorld();
-	char *str = "Hello, World";
+	char *str = "Hello, World!";
 	printf("%d\n", ft_strlen(str));
 	char *strCpy = malloc(ft_strlen(str) + 1);
 	strCpy = ft_strcpy(strCpy, str);
 	printf("Copied string: %s\n", strCpy);
+	char big[20];
+	printf("%s\n", ft_strcpy(big, str));
 
+	free(strCpy);
 	return 0;
 }
