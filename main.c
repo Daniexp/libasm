eb7398f5bc8e2c0d87e04ca7a04ccf330705cf7d
DaniExp <dani00exp@gmail.com>
2025-06-18 19:54:19 +0200

    Comment basic strdup test

--- main.c
+++ main.c
@@ -4,13 +4,16 @@
 
 int main() {
 	helloWorld();
+	//Test strlen
 	char *str = "Hello, World!\n";
 	printf("%d\n", ft_strlen(str));
+	//Test strcpy
 	char *strCpy = malloc(ft_strlen(str) + 1);
 	strCpy = ft_strcpy(strCpy, str);
 	printf("Copied string: %s", strCpy);
 	char big[20];
 	printf("%s\n", ft_strcpy(big, str));
+	//Test strcmp
 	char *str2 = malloc(ft_strlen(str) + 1);
 	str2 = 	ft_strcpy(str2, str);
 	printf("Comparison: %d\n", ft_strcmp(str, str2));
@@ -21,10 +24,18 @@ int main() {
 	free(strCpy);
 	free(str2);
 
+	//Test read
 	int fd = open("example.txt", O_RDONLY);
 	char buffer[6];
 	printf("Bytes readed: %d\n", ft_read(fd, buffer, 5));
 	buffer[5] = '\0';
 	printf("example.txt: %s\n", buffer);
+
+	//Test strdup
+	/*
+	strCpy = ft_strdup(str);
+	printf("Copied String: %d", strCpy);
+	free(strCpy);
+	*/
 	return 0;
 }
