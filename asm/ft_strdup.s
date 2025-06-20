ade8c39131a9c51efbd2a93d0580f22a29b25ac0
DaniExp <dani00exp@gmail.com>
2025-06-20 12:42:11 +0200

    Implement strdup with rbp stack convention

--- asm/ft_strdup.s
+++ asm/ft_strdup.s
@@ -1,9 +1,31 @@
 ;char *strdup(char *str)
+;void *malloc(size_t size)
 
 section .text
+	extern malloc
+	extern ft_strlen
+	extern ft_strcpy
 	global ft_strdup
 ft_strdup:
+	push rbp
+	mov rbp, rsp ; setup stack frame
 
+	push rdi ; save original string pointer
+
+	call ft_strlen
+	inc rax
+	mov rdi, rax ; size of the new string (including null terminator)
+
+	call malloc wrt ..plt
+	cmp rax, 0
+	je .end
+
+	mov rdi, rax ; new allocated memory address
+	pop rsi ; restore original string pointer
+	call ft_strcpy
+
+	mov rsp, rbp
+	pop rbp ; teardown stack frame
 .end:
 	ret
 
