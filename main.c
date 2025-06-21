8c8478b7a97412e97b77d110ca1d9ca955ae4785
DaniExp <dani00exp@gmail.com>
2025-06-21 16:16:46 +0200

    Add strcpy test

--- main.c
+++ main.c
@@ -19,28 +19,35 @@ void printHeader(char *str)
 void test_strlen(char *str)
 {
 	printHeader("ft_strlen");
-	printf("String -> %s", str);
+	printf("String -> %s\n", str);
 	printf("ft_strlen -> %d\n", ft_strlen(str));
 	printf("strlen -> %d\n", strlen(str));
 	printLine();
 }
 
-void test_strCpy(char *str)
+void test_strcpy(char *str)
 {
+	char *strCpy = malloc(strlen(str) + 1);
+	char *strCpy2 = malloc(strlen(str) + 1);
+
 	printHeader("ft_strCpy");
+	printf("Original -> %s\n", str);
+	printf("ft_strcpy -> %s\n", ft_strcpy(strCpy, str));
+	printf("strcpy -> %s\n", strcpy(strCpy2, str));
+	printLine();
+
+	free(strCpy);
+	free(strCpy2);
 }
 
 int main() {
 	//Test strlen
-	char *str = "Hello, World!\n";
-	test_strlen(str);
+	test_strlen("Hello, World!");
 	test_strlen("");
 	//Test strcpy
-	char *strCpy = malloc(ft_strlen(str) + 1);
-	strCpy = ft_strcpy(strCpy, str);
-	printf("Copied string: %s", strCpy);
-	char big[20];
-	printf("%s\n", ft_strcpy(big, str));
+	test_strcpy("Hello, World!");
+	test_strcpy("");
+	/*
 	//Test strcmp
 	char *str2 = malloc(ft_strlen(str) + 1);
 	str2 = 	ft_strcpy(str2, str);
@@ -49,8 +56,6 @@ int main() {
 	//Test write
 	ft_write(1, str, ft_strlen(str));
 	
-	free(strCpy);
-	free(str2);
 
 	//Test read
 	int fd = open("example.txt", O_RDONLY);
@@ -63,5 +68,6 @@ int main() {
 	strCpy = ft_strdup(str);
 	printf("Copied String: %s", strCpy);
 	free(strCpy);
+	*/
 	return 0;
 }
